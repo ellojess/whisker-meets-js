579d8cf8c6c9d681e7d6dd603ad7b4e099ad9a95
Jess <jtrinh619@gmail.com>
2020-07-09 18:22:42 -0700

    setup passport

---
+++ index.js
@@ -0,0 +1,27 @@
+/*  EXPRESS SETUP  */
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+const bodyParser = require('body-parser');
+const expressSession = require('express-session')({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('App listening on port ' + port));
+
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
+
+app.use(passport.initialize());
+app.use(passport.session());
