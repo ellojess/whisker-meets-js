4b85549e27531ed35f8f6ab3a5bcfb06fe8aa65d
Jess <jtrinh619@gmail.com>
2020-12-08 13:06:23 -0800

    clean up code -- successfully Dockerized

--- db/whiskersdb.js
+++ db/whiskersdb.js
@@ -2,13 +2,18 @@
 const mongoose = require("mongoose");
 assert = require("assert");
 
-// const url = process.env.MONGODB_URI || "mongodb://localhost/whiskers-db";
-const url = process.env.MONGODB_URI || "mongodb://mongo:270217/whiskers-db";
+let url = "mongodb://host.docker.internal:27017/whiskers-db";
+url += "?retryWrites=true&w=majority"
+console.log("//////////////////", url)
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
   url,
-  { useNewUrlParser: true },
+  { useNewUrlParser: true,
+    authSource: "admin",
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,},
   function(err, db) {
     assert.equal(null, err);
     console.log("Connected successfully to database");
@@ -16,13 +21,6 @@ mongoose.connect(
     // db.close(); turn on for testing
   }
 );
-// mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
-
-mongoose.connect('mongodb://localhost/testdb').then(() => {
-console.log("Connected to Database");
-}).catch((err) => {
-    console.log("Not Connected to Database ERROR! ", err);
-});
 
 mongoose.set("debug", true);
 
